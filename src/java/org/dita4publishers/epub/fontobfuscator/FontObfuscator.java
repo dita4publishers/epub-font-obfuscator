2290a71b42063ea0db6f0bb86a6049ad4dfb7849
Eliot Kimber <ekimber@contrext.com>
2016-04-19 10:17:11 -0500

    Setting up the project

---
+++ src/java/org/dita4publishers/epub/fontobfuscator/FontObfuscator.java
@@ -0,0 +1,17 @@
+package org.dita4publishers.epub.fontobfuscator;
+
+/**
+ * Application class for applying EPUB3 font obfuscation to font
+ * files.
+ * 
+ * 
+ *
+ */
+public class FontObfuscator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
